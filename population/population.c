b4e1d36ee714e0ef423b5b4184880174d921f749
omar-232 <143813505+omar-232@users.noreply.github.com>
2023-09-03 06:01:30 +0000

    Sun, Sep 3, 2023, 8:01 AM +02:00

--- population/population.c
+++ population/population.c
@@ -3,11 +3,33 @@
 
 int main(void)
 {
-    // TODO: Prompt for start size
+    int start_size = 0;
+    int end_size = 0;
+    int years = 0;
 
-    // TODO: Prompt for end size
+    // Prompt for start size
+    while (start_size < 9)
+    {
+        printf("Enter the starting population size (must be at least 9): ");
+        scanf("%d", &start_size);
+    }
+    // Prompt for end size
+    while (end_size < start_size)
+    {
+        printf("Enter the ending population size (must be greater than or equal to the starting size): ");
+        scanf("%d", &end_size);
+    }
 
-    // TODO: Calculate number of years until we reach threshold
+    // Calculate the number of years
+    while (start_size < end_size)
+    {
+        start_size += (start_size / 3) - (start_size / 4);
+        years += 1;
+    }
 
-    // TODO: Print number of years
+    // Print the number of years
+    printf("Years: %d\n", years);
+
+    return 0;
 }
+

f7954d3c6a1da3331b35c8289b679dfa85dc9dc9
omar-232 <143813505+omar-232@users.noreply.github.com>
2023-09-04 08:25:52 +0000

    Mon, Sep 4, 2023, 10:25 AM +02:00

---
+++ bank/bank.py
@@ -0,0 +1,12 @@
+input = input("Greeting: ")
+
+if input == "Hello":
+    print("$0")
+if input == "Hello, Newman":
+     print("$0")
+if input == "Hey":
+     print("$20")
+if input == "How you doing?":
+     print("$20")
+if input == "What's happening?":
+     print("$100")

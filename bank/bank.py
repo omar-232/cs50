c2cbd5ac49180952acc1ab42dc4dacde35f4a25a
omar-232 <143813505+omar-232@users.noreply.github.com>
2023-09-04 08:35:17 +0000

    Mon, Sep 4, 2023, 10:35 AM +02:00

--- bank/bank.py
+++ bank/bank.py
@@ -4,7 +4,7 @@ if input == "Hello" or input == "Hello, Newman":
      print("$0")
 elif input == "Hey" or input == "How you doing?":
      print("$20")
-elif input == "What's happening?" or input of "What's up?":
+elif input == "What's happening?" or input == "What's up?":
      print("$100")
 else:
     print("No matching response")

1f030e69340d0f1bd3b853b1b753e02bbe50dce4
omar-232 <143813505+omar-232@users.noreply.github.com>
2023-09-04 23:02:01 +0000

    Tue, Sep 5, 2023, 1:02 AM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,30 +1,6 @@
-import subprocess
+from pyfiglet import Figlet
 
-# Function to run figlet.py with the specified font and input text
-def run_figlet(font, text):
-    try:
-        # Run figlet.py with the specified font and input text
-        result = subprocess.run(["python", "figlet.py", "-f", font], input=text, capture_output=True, text=True, check=True)
-        return result.stdout
-    except subprocess.CalledProcessError:
-        return "Error: Invalid usage"
-
-# Prompt the user for input text
-input_text = input("Enter text: ")
-
-# Determine which font to use based on input
-if "CS50" in input_text:
-    font = "slant"
-elif "Hello, world" in input_text:
-    font = "rectangles"
-elif "Moo" in input_text:
-    font = "alphabet"
-else:
-    font = "standard"
-
-# Run figlet.py with the determined font and input text
-output = run_figlet(font, input_text)
-
-# Print the output
-print(output)
-
+figlet = Figlet()
+figlet.getFonts()
+figlet.setFont(font=f)
+print(figlet.renderText(s))

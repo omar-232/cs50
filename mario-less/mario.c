0cbab54eb4f0ce1a0dcd14588395fab5c2da9400
omar-232 <143813505+omar-232@users.noreply.github.com>
2023-09-12 06:09:24 +0000

    Tue, Sep 12, 2023, 8:09 AM +02:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -5,19 +5,16 @@ int main() {
     int i, j, k;
 
     do {
-    printf("Enter the height of the pattern: ");
-    scanf("%d", &height);
-    } while (height > 1 && height < 9);
+        printf("Enter the height of the pattern (between 1 and 8): ");
+        scanf("%d", &height);
+    } while (height < 1 || height > 8);
 
-    for (i = 0; i < height; i++)
-    {
-        for (j = 0; j < height - i - 1; j++)
-        {
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < height - i - 1; j++) {
             printf(" ");
         }
 
-        for (k = 0; k < i + 1; k++)
-        {
+        for (k = 0; k < i + 1; k++) {
             printf("#");
         }
         printf("\n");

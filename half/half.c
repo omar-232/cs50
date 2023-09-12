f9d8b6b453836db0a43ffe424f0b003ebb4f86cf
omar-232 <143813505+omar-232@users.noreply.github.com>
2023-09-12 04:31:29 +0000

    Tue, Sep 12, 2023, 6:31 AM +02:00

--- half/half.c
+++ half/half.c
@@ -11,16 +11,17 @@ int main(void)
     float bill_amount = get_float("Bill before tax and tip: ");
     float tax_percent = get_float("Sale Tax Percent: ");
     int tip_percent = get_int("Tip percent: ");
+    
     float Amount_each_person_owes = calculate_half(bill_amount, tax_percent, tip_percent);
-    printf("You will owe $%.2f each!\n", half(bill_amount, tax_percent, tip_percent));
+    printf("You will owe $%.2f each!\n", amount_each_person_owes);
 }
 
 // TODO: Complete the function
-float half(float bill, float tax, int tip)
+float calculate_half(float bill, float tax, int tip)
 {
-    float Sales_tax = bill + tax;
-    float Tip = bill + tip;
+    float Sales_tax = (bill + tax) / 100;
+    float Tip = (bill + tip) / 100;
     float Total_bill = bill + Sales_tax + Tip;
     float Amount_each_person_owes = Total_bill / 2;
-    return 0.0;
+    return (Amount_each_person_owes);
 }

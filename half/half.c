4fee6299069e1a4cf32acb6547d20e5c5aff9e0c
omar-232 <143813505+omar-232@users.noreply.github.com>
2023-09-12 04:34:29 +0000

    Tue, Sep 12, 2023, 6:34 AM +02:00

--- half/half.c
+++ half/half.c
@@ -11,8 +11,9 @@ int main(void)
     float bill_amount = get_float("Bill before tax and tip: ");
     float tax_percent = get_float("Sale Tax Percent: ");
     int tip_percent = get_int("Tip percent: ");
-    
+
     float Amount_each_person_owes = calculate_half(bill_amount, tax_percent, tip_percent);
+
     printf("You will owe $%.2f each!\n", amount_each_person_owes);
 }
 

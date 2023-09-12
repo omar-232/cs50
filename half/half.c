2ce21c2576cc18d0a9c3c9a7221b1ff3cd2e4694
omar-232 <143813505+omar-232@users.noreply.github.com>
2023-09-12 04:35:48 +0000

    Tue, Sep 12, 2023, 6:35 AM +02:00

--- half/half.c
+++ half/half.c
@@ -1,10 +1,7 @@
-// Calculate your half of a restaurant bill
-// Data types, operations, type casting, return value
-
 #include <cs50.h>
 #include <stdio.h>
 
-float half(float bill, float tax, int tip);
+float calculate_half(float bill, float tax, int tip);
 
 int main(void)
 {
@@ -12,17 +9,23 @@ int main(void)
     float tax_percent = get_float("Sale Tax Percent: ");
     int tip_percent = get_int("Tip percent: ");
 
-    float Amount_each_person_owes = calculate_half(bill_amount, tax_percent, tip_percent);
-
+    float amount_each_person_owes = calculate_half(bill_amount, tax_percent, tip_percent);
     printf("You will owe $%.2f each!\n", amount_each_person_owes);
 }
 
-// TODO: Complete the function
 float calculate_half(float bill, float tax, int tip)
 {
-    float Sales_tax = (bill + tax) / 100;
-    float Tip = (bill + tip) / 100;
-    float Total_bill = bill + Sales_tax + Tip;
-    float Amount_each_person_owes = Total_bill / 2;
-    return (Amount_each_person_owes);
+    // Calculate sales tax
+    float sales_tax = (bill * tax) / 100;
+
+    // Calculate tip
+    float tip_amount = (bill * tip) / 100;
+
+    // Calculate total bill
+    float total_bill = bill + sales_tax + tip_amount;
+
+    // Calculate the amount each person owes
+    float amount_each_person_owes = total_bill / 2;
+
+    return amount_each_person_owes;
 }
